91b2156ceeef92ebc601c701209deeb6904035d1
Moises Rabelo de Lima <95274316115@minc.intra>
2017-03-16 16:34:18 -0300

    inicial commit

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+// adicionando css que faz a função do divi custom css (Carrega por último)
+function divi_child_enqueue_styles() {
+    wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/css/custom.css');
+    wp_enqueue_style( 'alto-contraste', get_stylesheet_directory_uri() . '/css/alto-contraste.css');
+    wp_enqueue_script( 'alto-contraste', get_stylesheet_directory_uri() . '/js/alto-contraste.js');
+}
+
+add_action( 'wp_head', 'divi_child_enqueue_styles' );
+
+?>
